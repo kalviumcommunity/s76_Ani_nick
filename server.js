05ef15ef010e83ef9246342e8c01a79ae71ae7c4
balaji-r-2007 <balaji648balaji@gmail.com>
2025-02-12 11:01:26 +0530

    add db connection status

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 require('dotenv').config(); 
+// const getConnection = require('./database');
 
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const dotenv =require("dotenv");
-const connectDatabase = require('./database');
+const { connectDatabase, getConnection } = require('./database');
+
+connectDatabase();
 
 app.get('/ping', (req, res) => {
     res.send('Pong!');
 });
 
-connectDatabase();
+app.get('/', (req, res) => {
+    console.log(getConnection());
+    res.send(`Database is ${getConnection()}`);
+});
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
